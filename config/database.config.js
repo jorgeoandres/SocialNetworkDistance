44aba71cb8384b8eaa1a6b21dfaec91b8ce55037
jorgeoandres <jorgeorozcosanchez@gmail.com>
2020-08-08 10:04:09 -0500

    Changing mongo string connections

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    urlAdmin: 'mongodb+srv://jorgeoandres:XXXXXXXXX@jorgeoandres.i3otv.mongodb.net/SocialNetworkDistance?retryWrites=true&w=majority',
-    urlCommonUser: 'mongodb+srv://common:qeaIfAqaYkABt30g@jorgeoandres.i3otv.mongodb.net/SocialNetworkDistance?retryWrites=true&w=majority'
+    urlAdmin: 'MONGODBCONECTION',
+    urlCommonUser: 'MONGODBCONECTION'
 }
